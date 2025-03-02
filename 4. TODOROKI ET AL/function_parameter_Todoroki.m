7477f075fc43ce3ab543e933be1dbcb1c8e97904
Asli Karacelik <158611682+asli-karacelik@users.noreply.github.com>
2025-03-02 16:02:38 +0100

    Update function_parameter_Todoroki.m

--- 4. TODOROKI ET AL/function_parameter_Todoroki.m
+++ 4. TODOROKI ET AL/function_parameter_Todoroki.m
@@ -1,7 +1,7 @@
 function par = function_parameter_Todoroki(S)
 %% Parameters
 % Input parameters
-par.S = S;                           % Ratio of the height of the vertical pipe to height of the reservoir
+par.S = S;                           % Submergence ratio is the ratio of the reservoir height to the vertical pipe height
 par.D = 2.83;                        % Diameter of the vertical pipe (cm)
 par.H = 750;                         % Height of the vertical pipe (cm)
 par.rhol = 0.99821;                  % Liquid density (g/cm3) at 20 °C
